80cac0c250833e7f1f90a773d51e5e398bb8bc77
pppp2 <pjh9736@gmail.com>
2019-11-06 18:01:10 +0900

    2019|11|06

---
+++ 06_Vue/02_vue_webpack/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>여기는 최 상 위컴포 넌 트 입니.</h1>
+    <TodoList category="취준"></TodoList>
+    <TodoList category="SSAFY"></TodoList>
+    <TodoList category="기타"></TodoList>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default{
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+</style>
--- 06_Vue/02_vue_webpack/webpack.config.js
+++ 06_Vue/02_vue_webpack/webpack.config.js
@@ -1,12 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   // 여러개의 js 파일의 시작점 ( 웹팩이 파일을 읽기 시작하는 지점)
-  entry: {},
+  entry: {
+    app: path.join(__dirname, 'src', 'main.js')
+  },
   // 웹팩은 js만 변환이 가능하기 때문에 CSS, HTML 같은 모듈을 통해서 웹패이 이해할 수 있는것으로 변화 ㄴ
-  module: {},
+  module: {
+    rules:[
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader','css-loader'],
+      },
+    ]
+  },
   // 웹팩을 통해서 번들된 결과물을 추가적으로 처리하는 부분 (옵션)
-  plugins: [],
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
   // 여러 개의 js 파일을 하나로 만들어낸 결과물.
-  output: {},
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist')
+  },
 }
